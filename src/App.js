cf3c741c81e97f040deff845f2258b07056aceb8
unknown <wajidjaved@live.com>
2021-03-24 02:13:48 +0500

    important NOT important

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useMemo, useCallback } from "react";
 const App = () => {
   const [inputList, setInputList] = useState("");
+  const [isImportant, setIsImportant] = useState("0");
   const [Items, setItems] = useState([]);
+
   const itemEvent = (event) => {
     setInputList(event.target.value);
   };
-
-  const listOfItems = () => {
+  const addItem = useCallback(() => {
+    console.log(isImportant);
     setItems((oldItems) => {
-      return [...oldItems, inputList];
+      return [
+        ...oldItems,
+        {
+          title: inputList,
+          isImportant: !!parseInt(isImportant),
+        },
+      ];
     });
-  };
+  }, [inputList, isImportant]);
 
   const deleteItems = (index) => {
     console.log(index);
@@ -30,7 +38,18 @@ const App = () => {
           <h1>Todo App</h1>
           <br />
           <input type="text" placeholder="Add a Items" onChange={itemEvent} />
-          <button onClick={listOfItems}>+</button>
+          <select
+            defaultValue={"false"}
+            value={isImportant}
+            onChange={(e) => {
+              let val = e.target.value;
+              setIsImportant(val);
+            }}
+          >
+            <option value={"0"}>Not Important </option>
+            <option value={"1"}>Important</option>
+          </select>
+          <button onClick={addItem}>+</button>
           <ol>
             {Items.map((itemvalue, index) => {
               return (
@@ -42,7 +61,10 @@ const App = () => {
                         return deleteItems(index);
                       }}
                     />
-                    <li key={index}>{itemvalue}</li>
+                    <li key={index}>
+                      {itemvalue.title}{" "}
+                      {Boolean(itemvalue.isImportant) ? "important" : ""}
+                    </li>
                   </div>
                 </>
               );
