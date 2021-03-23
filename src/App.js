f0f357d3459025296257c21c9bf2b29a6c942c38
unknown <wajidjaved@live.com>
2021-03-23 14:32:36 +0500

    layout and styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>Todo App</h1>
+          <br />
+          <input type="text" placeholder="Add a Items" />
+          <button>+</button>
+          <ol>
+            <li>Buy Apple</li>
+          </ol>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
